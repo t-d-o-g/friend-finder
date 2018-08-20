98282c72bfa67d4033954a8eadae4722b1352088
Travis Collins <trc229@gmail.com>
2018-08-19 23:26:06 -0400

    Implement randomized/shuffled questions

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use('/public', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
-app.use('/public', express.static(__dirname + '/node_modules/jquery/dist'));
-app.use('/public', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+app.use('/public/js', express.static(`${__dirname}/node_modules/bootstrap/dist/js`));
+app.use('/public/js', express.static(`${__dirname}/node_modules/jquery/dist`));
+app.use('/public/js', express.static(`${__dirname}/app/public/js`));
+app.use('/public/css', express.static(`${__dirname}/node_modules/bootstrap/dist/css`));
+app.use('/public/css', express.static(`${__dirname}/app/public/css`));
 
 require('./app/routes/apiRoutes')(app);
 require('./app/routes/htmlRoutes')(app);
